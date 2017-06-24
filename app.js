c2a58bf962fa576ca19c6980d9faa83e90aef300
Cody Daniels <codydaniels@gmail.com>
2017-06-24 16:28:57 -0700

    Add trading functionality and settings modal

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var books = require('google-books-search');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,8 +10,15 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/post', function(req, res) {
-  res.send(req.body);
+app.post('/books', function(req, res) {
+  books.search(req.body.query, function(error, results) {
+    if(!error) {
+      res.send(results);
+    } else {
+      console.error(error);
+      res.send(error);
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, function(){
--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,7 @@
 var provider = new firebase.auth.TwitterAuthProvider();
+var database = firebase.database();
 var loggedIn = false;
 var userId;
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    loggedIn = true;
-    userId = user.uid;
-    $('#login').html('Sign Out');
-  } else {
-    loggedIn = false;
-    $('#login').html('Log In with Twitter <i class="fa fa-twitter" aria-hidden="true"></i>');
-  }
-});
 
 function login(){
   if(!loggedIn){
@@ -35,3 +25,144 @@ function login(){
     });
   }
 }
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    loggedIn = true;
+    userId = user.uid;
+    $('.displayName').html(user.displayName);
+    $('#login').html('Sign Out <i class="fa fa-sign-out" aria-hidden="true"></i>');
+    $('.loggedIn').removeClass('hidden');
+    $('.prompt').removeClass('hidden');
+    $('.settings').removeClass('hidden');
+    database.ref('users/' + userId).once('value').then(function(snapshot) {
+      if (snapshot.val().hasOwnProperty('city')) {
+        $('#city')[0].value = snapshot.val().city;
+      }
+      if (snapshot.val().hasOwnProperty('state')) {
+        $('#state')[0].value = snapshot.val().state;
+      }
+    });
+    database.ref('books/').once('value').then(function(snapshot) {
+      for(var key in snapshot.val()) {
+        if (snapshot.val().hasOwnProperty(key)) {
+          var book = snapshot.val()[key];
+          if(book.lender === userId) {
+            $('.myBooks').append('<a href="#" class="thumbnail book" id="my-' + key + '" onclick="removeBook(this.id)">'+
+              '<img src="' + book.thumbnail + '" alt="' + book.title + '">'+
+            '</a>');
+          }
+          if(book.borrower === userId && book.request === "confirmed") {
+            $('.tradedBooks').append('<a href="#" class="thumbnail book" id="traded-' + key + '" onclick="returnBook(this.id)">'+
+              '<img src="' + book.thumbnail + '" alt="' + book.title + '">'+
+            '</a>');
+          }
+          if(book.lender === userId && book.request === "pending") {
+            $('.tradeRequests').append('<a href="#" class="thumbnail book" id="request-' + key + '" onclick="confirmTrade(this.id)">'+
+              '<img src="' + book.thumbnail + '" alt="' + book.title + '">'+
+            '</a>');
+          }
+        }
+      }
+    });
+  } else {
+    loggedIn = false;
+    $('#login').html('Log In with Twitter <i class="fa fa-twitter" aria-hidden="true"></i>');
+    $('.loggedIn').addClass('hidden');
+    $('.prompt').addClass('hidden');
+    $('.settings').addClass('hidden');
+    $('.myBooks').html('');
+    $('.tradeRequests').html('');
+    $('.tradedBooks').html('');
+  }
+});
+
+// Update user settings in modal
+function updateSettings() {
+  var updates = {
+    city: $('#city')[0].value,
+    state: $('#state')[0].value
+  };
+  database.ref('users/' + userId).update(updates);
+}
+
+// Add book to available books and to user's library
+function addBook() {
+  $.post('/books', {
+    query: $('#bookTitle')[0].value
+  }, function(results) {
+    $('#bookTitle')[0].value = '';
+    database.ref('books/' + results[0].id).set({
+      title: results[0].title,
+      thumbnail: results[0].thumbnail,
+      lender: userId,
+      borrower: '',
+      request: 'none'
+    });
+    var book = results[0];
+    $('.available').append('<a href="#" class="thumbnail book" id="available-' + book.id + '" onclick="requestBook(this.id)">'+
+      '<img src="' + book.thumbnail + '" alt="' + book.title + '">'+
+    '</a>');
+    $('.myBooks').append('<a href="#" class="thumbnail book" id="my-' + book.id + '" onclick="removeBook(this.id)">'+
+      '<img src="' + book.thumbnail + '" alt="' + book.title + '">'+
+    '</a>');
+  });
+}
+
+// Remove a book from one's own library
+function removeBook(id) {
+  id = id.split('-')[1];
+  $('#available-' + id).remove();
+  $('#my-' + id).remove();
+  $('#request-' + id).remove();
+  $('#traded-' + id).remove();
+  database.ref('books/' + id).remove();
+}
+
+// Request a book from someone else's library
+function requestBook(id) {
+  if(loggedIn) {
+    id = id.split('-')[1];
+    $('#available-' + id).remove();
+    var updates = {
+      borrower: userId,
+      request: 'pending'
+    };
+    database.ref('books/' + id).update(updates);
+  }
+}
+
+// Confirm a trade request
+function confirmTrade(id) {
+  id = id.split('-')[1];
+  $('#request-' + id).remove();
+  var updates = {
+    request: 'confirmed'
+  };
+  database.ref('books/' + id).update(updates);
+}
+
+// Return a book to its owner
+function returnBook(id) {
+  id = id.split('-')[1];
+  $('#traded-' + id).remove();
+  var updates = {
+    borrower: '',
+    request: 'none'
+  };
+  database.ref('books/' + id).update(updates);
+}
+
+// Print available books to page upon visit
+database.ref('books/').once('value').then(function(snapshot) {
+  for(var key in snapshot.val()) {
+    if (snapshot.val().hasOwnProperty(key)) {
+      var book = snapshot.val()[key];
+      if(book.borrower === '') {
+        $('.available').append('<a href="#" class="thumbnail book" id="available-' + key + '" onclick="requestBook(this.id)">'+
+          '<img src="' + book.thumbnail + '" alt="' + book.title + '">'+
+        '</a>');
+      }
+    }
+  }
+});
